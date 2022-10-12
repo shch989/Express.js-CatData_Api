5d0dce44c5b09dcfdabebb38e1bfd0899ba1839b
shch989 <tankpop11@gmail.com>
2022-10-12 15:30:05 +0900

    middleware 실습

--- dist/app.js
+++ dist/app.js
@@ -3,10 +3,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
-var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log('this is logging middleware');
+    next();
+});
+app.get('/cats/som', function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log('this is som middleware');
+    next();
+});
 app.get('/', function (req, res) {
-    console.log(req);
+    console.log(req.rawHeaders[1]);
     res.send({ cats: app_model_1.Cat });
+});
+app.get('/cats/blue', function (req, res) {
+    console.log(req.rawHeaders[1]);
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get('/cats/som', function (req, res) {
+    console.log(req.rawHeaders[1]);
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log('this is error middleware');
+    res.send({ error: '404 not found error' });
+    next();
 });
 app.listen(8000, function () {
     console.log("Example app listening on port http://localhost:8000");
--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,37 @@ import { Cat, CatType} from './app.model'
 
 const app: express.Express = express()
 
-const data = [1, 2, 3, 4]
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log(req.rawHeaders[1])
+  console.log('this is logging middleware')
+  next()
+})
+
+app.get('/cats/som',(req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log(req.rawHeaders[1])
+  console.log('this is som middleware')
+  next()
+})
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req)
+  console.log(req.rawHeaders[1])
   res.send({ cats: Cat })
+})
+
+app.get('/cats/blue', (req: express.Request, res: express.Response) => {
+  console.log(req.rawHeaders[1])
+  res.send({blue: Cat[0] })
+})
+
+app.get('/cats/som', (req: express.Request, res: express.Response) => {
+  console.log(req.rawHeaders[1])
+  res.send({som: Cat[1] })
+})
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log('this is error middleware')
+  res.send({error: '404 not found error'})
+  next()
 })
 
 app.listen(8000, () => {
