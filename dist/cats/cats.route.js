4f7418a458ccfbf9dc5210ad9a6750a0a893fe2a
shch989 <tankpop11@gmail.com>
2022-10-13 18:56:42 +0900

    Put Patch Delete 사용

--- dist/cats/cats.route.js
+++ dist/cats/cats.route.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var cats_model_1 = require("./cats.model");
 var express_1 = require("express");
@@ -48,6 +59,74 @@ router.post('/cats', function (req, res) {
         res.status(200).send({
             success: true,
             data: { data: data }
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.message
+        });
+    }
+});
+router.put('/cats/:id', function (req, res) {
+    try {
+        var params_2 = req.params;
+        var body_1 = req.body;
+        var result_1;
+        cats_model_1.Cat.forEach(function (cat) {
+            if (cat.id === params_2.id) {
+                cat = body_1;
+                result_1 = cat;
+            }
+        });
+        res.status(200).send({
+            success: true,
+            data: {
+                cat: result_1,
+            }
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.message
+        });
+    }
+});
+router.patch('/cats/:id', function (req, res) {
+    try {
+        var params_3 = req.params;
+        var body_2 = req.body;
+        var result_2;
+        cats_model_1.Cat.forEach(function (cat) {
+            if (cat.id === params_3.id) {
+                cat = __assign(__assign({}, cat), body_2);
+                result_2 = cat;
+            }
+        });
+        res.status(200).send({
+            success: true,
+            data: {
+                cat: result_2,
+            }
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.message
+        });
+    }
+});
+router.delete('/cats/:id', function (req, res) {
+    try {
+        var params_4 = req.params;
+        var newCat = cats_model_1.Cat.filter(function (cat) {
+            return cat.id !== params_4.id;
+        });
+        res.status(200).send({
+            success: true,
+            data: { newCat: newCat }
         });
     }
     catch (error) {
--- src/cats/cats.route.ts
+++ src/cats/cats.route.ts
@@ -61,4 +61,76 @@ router.post('/cats', (req, res) => {
   }
 })
 
+//* UPDATE 고양이 데이터 업데이트 -> PUT
+router.put('/cats/:id', (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if(cat.id === params.id) {
+        cat = body
+        result = cat;
+      }
+    })
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      }
+    })
+  } catch (error:any) {
+    res.status(400).send({
+      success: false,
+      error: error.message
+    })
+  }
+})
+
+//* UPDATE 고양이 데이터 부분적으로 업데이트 -> PATCH
+router.patch('/cats/:id', (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if(cat.id === params.id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    })
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      }
+    })
+  } catch (error:any) {
+    res.status(400).send({
+      success: false,
+      error: error.message
+    })
+  }
+})
+
+//* UPDATE 고양이 데이터 삭제 -> DELETE
+router.delete('/cats/:id', (req, res) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) =>
+      cat.id !== params.id
+    )
+
+    res.status(200).send({
+      success: true,
+      data: {newCat}
+    })
+  } catch (error:any) {
+    res.status(400).send({
+      success: false,
+      error: error.message
+    })
+  }
+})
+
 export default router
